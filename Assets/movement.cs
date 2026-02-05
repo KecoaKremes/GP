51bec65a789d1f0ac619b06dcb84376b8b148eb8
KecoaKremes <valencia8077@gmail.com>
2026-02-05 12:44:48 +0700

    commit die

---
+++ Assets/movement.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    private Animator animator;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+        setAnimation(moveInput);
+
+    }
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void setAnimation(float moveInput)
+    {
+        if (isGrounded)
+        {
+            if (moveInput == 0)
+            {
+                animator.Play("player_Idle");
+            }
+            else
+            {
+                animator.Play("player_Run");
+            }
+        }
+        else
+        {
+            if (rb.linearVelocityY > 0)
+            {
+                animator.Play("player_Jump");
+            }
+            else
+            {
+                animator.Play("Player_Fall");
+            }
+        }
+    }
+}
+
+
